f23520504cb514cb2135010bc224a925fed4259c
willicom2000 <willicom2000_9@hotmail.com>
2026-09-02 12:32:18 -0300

    [master] feat: upd samples mbsdr.

--- 2.3.0/example-classic-mbsdr/src/app/api/api-rest/facephi.service.ts
+++ 2.3.0/example-classic-mbsdr/src/app/api/api-rest/facephi.service.ts
@@ -9,7 +9,68 @@ export class FacephiService
 {
   constructor( private http: HttpClient ) { }
  
-  url: string   = 'https://api.xxx-xxx.xxx/iad';
+  url: string   = 'https://api.xxx-xxx.xxx/xxx';
+  async extractDocumentData( tokenOCR: string )
+  {
+    const options = {
+      url: this.url + '/services/extractDocumentData',
+      headers: { 'x-api-key': '', 'Content-Type': 'application/json; charset=UTF-8', },
+      data: { 'tokenOcr': tokenOCR }
+    };
+
+    console.log(options);
+    return CapacitorHttp.post(options);
+  }
+
+  async authenticalFacial(faceImage: string, bestImage: string)
+  {
+    const options = {
+      url: this.url + '/services/authenticateFacial',
+      headers: { 'x-api-key': '', 'Content-Type': 'application/json; charset=UTF-8', },
+      data: { 'token1': faceImage, 'token2': bestImage, 'method': 1 }
+    };
+
+    console.log(options);
+    return CapacitorHttp.post(options);
+  }
+
+  async documentValidation(rawFrontDocument: string, rawBackDocument: string)
+  {
+    const options = {
+      url: this.url + '/verify/documentValidation/v2/start',
+      headers: { 'x-api-key': '', 'Content-Type': 'application/json; charset=UTF-8', },
+      data: {
+        'country': "AR",
+        'idType': "ID_CARD",
+        'documentRawImageMimeType': 'image/jpeg',
+        'documentFrontRawImage': rawFrontDocument,
+        'documentBackRawImage': rawBackDocument 
+      }
+    };
+
+    console.log(options);
+    return CapacitorHttp.post(options);
+  }
+
+  async documentValidationStatus( 
+    maxIntentos: number         = 10, 
+    intervalo: number           = 2000, 
+    scanReferenceResult: string = '', 
+    typeResult: string          = '' 
+  )
+  {
+    const options = {
+      url: this.url + '/verify/documentValidation/v2/status',
+      headers: { 'x-api-key': '', 'Content-Type': 'application/json; charset=UTF-8', },
+      data: {
+        'scanReference': scanReferenceResult,
+        'type': typeResult,
+      }
+    };
+
+    console.log(options);
+    return CapacitorHttp.post(options);
+  }
 
   callIAD( iad: string ) 
   { 
--- 2.3.0/example-classic-mbsdr/src/app/constants.ts
+++ 2.3.0/example-classic-mbsdr/src/app/constants.ts
@@ -1,5 +1,7 @@
-export const CUSTOMER_ID            = "capacitor@facephi.com";
-export const LICENSE_URL            = "https://licensing.facephi.pro";
-export const LICENSE_STRING_ANDROID = "";
-export const LICENSE_STRING_IOS     = "{}";
-export const LICENSE_STRING_ANDROID = "{}";
+export const CUSTOMER_ID                = "capacitor@facephi.com";
+export const LICENSE_URL                = "https://xxx.xxx.xxx";
+export const APP_DARK_MODE_STORAGE_KEY  = 'app-dark-mode';
+export const LICENSE_APIKEY_ANDROID     = "";
+export const LICENSE_APIKEY_IOS         = "";
+export const LICENSE_STRING_IOS         = "{}";
+export const LICENSE_STRING_ANDROID     = "{}";
