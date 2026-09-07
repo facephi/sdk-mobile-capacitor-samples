8ba59ab698f316538a0d1c0f454732c976fb20cc
willicom2000 <willicom2000_9@hotmail.com>
2026-09-07 13:25:49 -0300

    Update app.module.ts

--- 2.11.0/example-nfc/src/app/app.module.ts
+++ 2.11.0/example-nfc/src/app/app.module.ts
@@ -4,15 +4,11 @@ import { RouteReuseStrategy } from '@angular/router';
 import { IonicModule, IonicRouteStrategy, Platform } from '@ionic/angular';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
-import { HttpClientModule } from '@angular/common/http';
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 import { CoreService } from './services/core/core.service';
 
-@NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, HttpClientModule],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
-  bootstrap: [AppComponent],
-})
+@NgModule({ declarations: [AppComponent],
+    bootstrap: [AppComponent], imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule], providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }, provideHttpClient(withInterceptorsFromDi())] })
 export class AppModule 
 {
   constructor(private platform: Platform, private coreService: CoreService) 
